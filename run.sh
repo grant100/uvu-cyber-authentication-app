9fee0760a93318e198e5e906f6c7c1e7d71830a4
root <root@localhost.localdomain>
2018-10-11 19:01:26 -0600

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -jar build/libs/app-uvu-cybersecurity-0.0.1-SNAPSHOT.jar -Dspring.config.location=file:./application.properties
+java -jar build/libs/uvu-cyber-authentication-app-0.0.1-SNAPSHOT.jar -Dspring.config.location=file:./application.properties
